36501d3cf535c05eb42abb463399e0f6f71b13cb
Stuart Kent <skent.dev@gmail.com>
2021-12-05 08:16:54 -0600

    Add day 05 setup

---
+++ src/Day05.kt
@@ -0,0 +1,29 @@
+import utils.readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = """
+        0,9 -> 5,9
+        8,0 -> 0,8
+        9,4 -> 3,4
+        2,2 -> 2,1
+        7,0 -> 7,4
+        6,4 -> 2,0
+        0,9 -> 2,9
+        3,4 -> 1,4
+        0,0 -> 8,8
+        5,5 -> 8,2
+    """.trimIndent().split('\n')
+
+    check(part1(testInput) == 5)
+
+    val input = readInput("Day05")
+    println(part1(input))
+}
