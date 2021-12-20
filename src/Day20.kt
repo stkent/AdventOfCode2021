502d766e2d3fe9f34b64582baf7bfd797b82dbda
Stuart Kent <skent.dev@gmail.com>
2021-12-20 12:36:23 -0500

    Add day 20 solutions

---
+++ src/Day20.kt
@@ -0,0 +1,116 @@
+import utils.GridPoint2d
+import utils.bounds
+import utils.extensions.collapseToString
+import utils.readInput
+
+typealias Point = GridPoint2d
+
+private const val LIT = '#'
+private const val UNLIT = '.'
+
+fun main() {
+    fun List<String>.litPixels(): Map<Point, Boolean> {
+        fun charAt(point: Point): Char? {
+            return this
+                .getOrNull(point.y)
+                ?.getOrNull(point.x)
+        }
+
+        return buildMap {
+            for (y in this@litPixels.indices) {
+                for (x in this@litPixels[0].indices) {
+                    val point = Point(x, y)
+                    put(point, charAt(point) == LIT)
+                }
+            }
+        }
+    }
+
+    fun List<String>.parse(): Pair<String, Map<Point, Boolean>> {
+        val algorithm = this.first()
+        val image = this.drop(2).litPixels()
+        return algorithm to image
+    }
+
+    algorithm@ fun String.enhance(image: Map<Point, Boolean>, iteration: Int): Map<Point, Boolean> {
+        val oldBounds = image.keys.bounds()
+        val xMin = oldBounds.xMin - 1
+        val xMax = oldBounds.xMax + 1
+        val yMin = oldBounds.yMin - 1
+        val yMax = oldBounds.yMax + 1
+
+        val outOfBoundsPixelsAreLit =
+            when {
+                // Never turned on:
+                first() == UNLIT -> false
+                // Turned on during the first enhancement, and never turned off:
+                first() == LIT && last() == LIT -> iteration > 1
+                // Turned on during off-numbered enhancements, and off during even-numbered enhancements:
+                first() == LIT && last() == UNLIT -> iteration % 2 == 0
+                else -> error("Should not reach here.")
+            }
+
+        return buildMap {
+            for (x in xMin..xMax) {
+                for (y in yMin..yMax) {
+                    val centerPoint = Point(x, y)
+
+                    val inputPoints =
+                        buildSet {
+                            add(centerPoint)
+                            addAll(centerPoint.surroundingPoints())
+                        }
+
+                    val index =
+                        inputPoints
+                            .sortedWith(compareBy(Point::y).thenBy(Point::x))
+                            .map { point ->
+                                when (point) {
+                                    in image -> if (image[point]!!) '1' else '0'
+                                    else -> if (outOfBoundsPixelsAreLit) '1' else '0'
+                                }
+                            }
+                            .collapseToString()
+                            .toInt(radix = 2)
+
+                    put(centerPoint, this@algorithm[index] == LIT)
+                }
+            }
+        }
+    }
+
+    fun List<String>.litPixelCount(enhancementCount: Int): Int {
+        val (algorithm, image) = this.parse()
+
+        return (1..enhancementCount)
+            .fold(image) { outputImage, iteration ->
+                algorithm.enhance(outputImage, iteration)
+            }
+            .values.count { it }
+    }
+
+    fun part1(input: List<String>): Int {
+        return input.litPixelCount(enhancementCount = 2)
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.litPixelCount(enhancementCount = 50)
+    }
+
+    val testInput = """
+        ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+        
+        #..#.
+        #....
+        ##..#
+        ..#..
+        ..###
+    """.trimIndent().split('\n')
+
+    check(part1(testInput) == 35)
+    check(part2(testInput) == 3351)
+
+    val input = readInput("Day20")
+    println(part1(input))
+    println(part2(input))
+}
