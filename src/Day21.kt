8253c3ff9ed8f44603757c350f3df4434496e325
Stuart Kent <stuart@stkent.com>
2021-12-21 11:50:52 -0500

    WIP day 21 part 2

--- src/Day21.kt
+++ src/Day21.kt
@@ -1,37 +1,108 @@
-fun main() {
-    fun part1(input: Map<Int, Int>): Int {
-        var turn = 1
-        val positions = input.toMutableMap()
-        val scores = mutableMapOf(1 to 0, 2 to 0)
+import kotlin.math.max
+import kotlin.math.min
 
-        fun play() {
-            val player = (turn - 1) % 2 + 1
-            val move = 3 * (3 * turn - 1)
-            val newPosition = (positions[player]!! + move - 1) % 10 + 1
-            positions[player] = newPosition
-            scores[player] = scores[player]!! + newPosition
-            turn += 1
+private class PlayerState(val place: Long, val score: Long) {
+    fun move(distance: Long): PlayerState {
+        val newPlace = (place + distance - 1) % 10 + 1
+        val newScore = score + newPlace
+        return PlayerState(place = newPlace, score = newScore)
+    }
+}
+
+private data class GameState(val p1: PlayerState, val p2: PlayerState, val p1Turn: Boolean) {
+    val maxScore = max(p1.score, p2.score)
+    val minScore = min(p1.score, p2.score)
+
+    fun applyMove(moveDistance: Long): GameState {
+        return GameState(
+            p1 = if (p1Turn) p1.move(moveDistance) else p1,
+            p2 = if (p1Turn) p2 else p2.move(moveDistance),
+            p1Turn = !p1Turn
+        )
+    }
+
+    companion object {
+        fun initial(places: Pair<Long, Long>): GameState {
+            return GameState(
+                p1 = PlayerState(place = places.first, score = 0),
+                p2 = PlayerState(place = places.second, score = 0),
+                p1Turn = true
+            )
+        }
+    }
+}
+
+private class DeterministicDie {
+    var rollCount: Long = 0
+        private set
+
+    fun sumOfNextThreeRolls(): Long {
+        val result = 3 * (rollCount + 2)
+        rollCount += 3
+        return result
+    }
+}
+
+private class Wins(val p1: Long, val p2: Long) {
+    operator fun plus(other: Wins) = Wins(p1 + other.p1, p2 + other.p2)
+}
+
+private operator fun Long.times(wins: Wins) = Wins(this * wins.p1, this * wins.p2)
+
+fun main() {
+    fun part1(input: Pair<Long, Long>): Long {
+        var state = GameState.initial(places = input)
+        val die = DeterministicDie()
+
+        while (state.maxScore < 1000) {
+            val moveDistance = die.sumOfNextThreeRolls()
+            state = state.applyMove(moveDistance)
         }
 
-        while (true) {
-            play()
-            if (scores.any { (_, score) -> score >= 1000 }) {
-                val lowScore = scores.values.minOrNull()!!
-                val rolls = 3 * (turn - 1)
-                return lowScore * rolls
+        return state.minScore * die.rollCount
+    }
+
+    val quantumDieDistanceFrequencies =
+        mapOf(
+            3L to 1L,
+            4L to 3L,
+            5L to 6L,
+            6L to 7L,
+            7L to 6L,
+            8L to 3L,
+            9L to 1L
+        )
+
+    fun part2Wins(state: GameState, cache: MutableMap<GameState, Wins>): Wins {
+        return when {
+            state.maxScore >= 21 ->
+                if (state.p1.score > state.p2.score) Wins(1, 0) else Wins(0, 1)
+
+            state in cache -> cache[state]!!
+
+            else -> {
+                quantumDieDistanceFrequencies
+                    .map { (distance, frequency) ->
+                        frequency * part2Wins(state.applyMove(distance), cache)
+                    }
+                    .reduce(Wins::plus)
+                    .also { wins -> cache[state] = wins }
             }
         }
     }
 
-    fun part2(input: Map<Int, Int>): Long {
-        return 0
+    fun part2(input: Pair<Long, Long>): Long {
+        val cache = mutableMapOf<GameState, Wins>()
+        val wins = part2Wins(GameState.initial(places = input), cache)
+        return max(wins.p1, wins.p2)
     }
 
-    val testInput = mapOf(1 to 4, 2 to 8)
+    val testInput = 4L to 8L
 
-    check(part1(testInput) == 739785)
-//    check(part2(testInput) == 444356092776315L)
+    check(part1(testInput) == 739785L)
+    check(part2(testInput) == 444356092776315L)
 
-    val input = mapOf(1 to 7, 2 to 1)
+    val input = 7L to 1L
     println(part1(input))
+    println(part2(input))
 }
